3b9d21e03ab58bd2e943d980e56e34f72ca3dad3
adam <abigserve@gmail.com>
2019-01-19 22:30:00 +1100

    Started on UPDATE parsing

---
+++ bgp/update.go
@@ -0,0 +1,22 @@
+package bgp
+
+import "net"
+
+// Routes removed from this BGP relationship
+type WithdrawnRoutes struct {
+	Length uint16
+	Routes []WithdrawnRoute
+}
+type WithdrawnRoute struct {
+	Length uint8
+	Prefix net.IP
+}
+
+// BGP Path attributes (AS-PATH, Origin, MED, etc.)
+type PathAttributes struct {
+	Length     uint16
+	Attributes []PathAttribute
+}
+
+type PathAttribute struct {
+}
